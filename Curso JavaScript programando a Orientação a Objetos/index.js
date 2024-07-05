9906a8b151b0a2b67147d84b5d06f7d7ceb1401f
Mayara Plaza <74818185+MayaraPlaza@users.noreply.github.com>
2024-07-05 17:18:55 -0300

    Add files via upload

---
+++ Curso JavaScript programando a Orientação a Objetos/index.js
@@ -0,0 +1,23 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 88844455509;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+cliente1.rg = 1546859574
+
+cliente2.nome = "Alice";
+cliente2.cpf = 22266655509;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1, cliente2);
